c263c1494ab077d4c2269660a96f602d07d33a4e
Andrew Russo <russspruce@gmail.com>
2016-07-17 20:39:21 -0700

    Created base for OKHttp API path to allow the program to connect to the API database.

---
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.andrewrusso.elforge;
+
+/**
+ * Created by andrewrusso on 7/17/16.
+ */
+public class Constants {
+
+    public static final String GAME_DB_API_KEY = BuildConfig.GAME_DB_API_KEY;
+}
---
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/services/GameService.java
@@ -0,0 +1,22 @@
+package com.epicodus.andrewrusso.elforge.services;
+
+/**
+ * Created by andrewrusso on 7/17/16.
+// */
+//
+//import okhttp3.Call;
+//import okhttp3.Callback;
+//import okhttp3.HttpUrl;
+//import okhttp3.OkHttpClient;
+//import okhttp3.Request;
+//import okhttp3.Response;
+//
+//
+//public class GameService {
+//    public static void findGames(String name, Callback callback) {
+//        OkHttpClient client = new OkHttpClient.Builder().build();
+//
+//        String url =
+//
+//    }
+//}
