3e2b69b7ff358a0e0be412e61673c22bd1274333
Andrew Russo <russspruce@gmail.com>
2016-07-19 03:42:18 -0700

    Added API key and service for Game API request.  Currently need to work on getting the API call to work in order to be able to proplery place the Key within the URL request.

--- app/src/main/java/com/epicodus/andrewrusso/elforge/services/GameService.java
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/services/GameService.java
@@ -3,20 +3,31 @@ package com.epicodus.andrewrusso.elforge.services;
 /**
  * Created by andrewrusso on 7/17/16.
 // */
-//
-//import okhttp3.Call;
-//import okhttp3.Callback;
-//import okhttp3.HttpUrl;
-//import okhttp3.OkHttpClient;
-//import okhttp3.Request;
-//import okhttp3.Response;
-//
-//
-//public class GameService {
-//    public static void findGames(String name, Callback callback) {
-//        OkHttpClient client = new OkHttpClient.Builder().build();
-//
-//        String url =
-//
-//    }
-//}
+
+
+import android.util.Log;
+
+import okhttp3.Call;
+        import okhttp3.Callback;
+        import okhttp3.HttpUrl;
+        import okhttp3.OkHttpClient;
+        import okhttp3.Request;
+        import okhttp3.Response;
+
+
+public class GameService {
+    public static void findGames(String gameListTextView, Callback callback) {
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+//        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.GAME_DB_API_KEY).newBuilder();
+//        urlBuilder.addQueryParameter("query", gameListTextView);
+//        urlBuilder.addQueryParameter("api_key", Constants.GAME_DB_API_KEY);
+        String url = urlBuilder.build().toString();
+        Log.d("url", url);
+
+        Request request = new Request.Builder().url(url).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
---
+++ app/src/main/res/Constants.java
@@ -0,0 +1,5 @@
+public class Constants {
+    public static final String GAME_DB_API_KEY = BuildConfig.GAME_DB_API_KEY;
+    public static final String GAME_NAME_TITLE = "gamename";
+    public static final String GAME_BASE_URL = "http://www.giantbomb.com/api/search/?api_key="+GAME_DB_API_KEY+ "&format=json&query="+GAME_NAME_TITLE+"&resources=game";
+}
